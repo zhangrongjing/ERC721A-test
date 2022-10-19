1e6a2ddb787c78b0fd1ae3f1b4057dc0792a454f
zhangrongjing <114322287+zhangrongjing@users.noreply.github.com>
2022-10-19 22:44:55 +0800

    Update NFTERC721A.sol

--- NFTERC721A.sol
+++ NFTERC721A.sol
@@ -1,91 +1,84 @@
 // SPDX-License-Identifier: MIT
 
-// Amended by @QatarWorldCup_
+pragma solidity >=0.8.9 <0.9.0;
 
-pragma solidity >=0.7.0 <0.9.0;
+import 'erc721a/contracts/extensions/ERC721AQueryable.sol';
+import '@openzeppelin/contracts/access/Ownable.sol';
+import '@openzeppelin/contracts/utils/cryptography/MerkleProof.sol';
+import '@openzeppelin/contracts/security/ReentrancyGuard.sol';
+import '@openzeppelin/contracts/utils/Strings.sol';
 
-import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
-import "@openzeppelin/contracts/utils/Counters.sol";
-import "@openzeppelin/contracts/access/Ownable.sol";
+contract YourNftToken is ERC721AQueryable, Ownable, ReentrancyGuard {
 
-contract WorldCupNFT is ERC721, Ownable {
   using Strings for uint256;
-  using Counters for Counters.Counter;
 
-  Counters.Counter private supply;
+  bytes32 public merkleRoot;
+  mapping(address => bool) public whitelistClaimed;
 
-  string public uriPrefix = "";
-  string public uriSuffix = ".json";
+  string public uriPrefix = '';
+  string public uriSuffix = '.json';
   string public hiddenMetadataUri;
   
-  uint256 public cost = 0 ether;
-  uint256 public maxSupply = 5666;
-  uint256 public maxMintAmountPerTx = 5;
+  uint256 public cost;
+  uint256 public maxSupply;
+  uint256 public maxMintAmountPerTx;
 
   bool public paused = true;
+  bool public whitelistMintEnabled = false;
   bool public revealed = false;
 
-  constructor() ERC721("21", "22") {
-    setHiddenMetadataUri("ipfs://QmSVMD7HHa7Wa3h9JcRGrFDzB44y4RBbmp5aRjLY5qUi2n");
+  constructor(
+    string memory _tokenName,
+    string memory _tokenSymbol,
+    uint256 _cost,
+    uint256 _maxSupply,
+    uint256 _maxMintAmountPerTx,
+    string memory _hiddenMetadataUri
+  ) ERC721A(_tokenName, _tokenSymbol) {
+    setCost(_cost);
+    maxSupply = _maxSupply;
+    setMaxMintAmountPerTx(_maxMintAmountPerTx);
+    setHiddenMetadataUri(_hiddenMetadataUri);
   }
 
   modifier mintCompliance(uint256 _mintAmount) {
-    require(_mintAmount > 0 && _mintAmount <= maxMintAmountPerTx, "Invalid mint amount!");
-    require(supply.current() + _mintAmount <= maxSupply, "Max supply exceeded!");
+    require(_mintAmount > 0 && _mintAmount <= maxMintAmountPerTx, 'Invalid mint amount!');
+    require(totalSupply() + _mintAmount <= maxSupply, 'Max supply exceeded!');
     _;
   }
 
-  function totalSupply() public view returns (uint256) {
-    return supply.current();
+  modifier mintPriceCompliance(uint256 _mintAmount) {
+    require(msg.value >= cost * _mintAmount, 'Insufficient funds!');
+    _;
   }
 
-  function mint(uint256 _mintAmount) public payable mintCompliance(_mintAmount) {
-    require(!paused, "The contract is paused!");
-    require(msg.value >= cost * _mintAmount, "Insufficient funds!");
+  function whitelistMint(uint256 _mintAmount, bytes32[] calldata _merkleProof) public payable mintCompliance(_mintAmount) mintPriceCompliance(_mintAmount) {
+    // Verify whitelist requirements
+    require(whitelistMintEnabled, 'The whitelist sale is not enabled!');
+    require(!whitelistClaimed[_msgSender()], 'Address already claimed!');
+    bytes32 leaf = keccak256(abi.encodePacked(_msgSender()));
+    require(MerkleProof.verify(_merkleProof, merkleRoot, leaf), 'Invalid proof!');
 
-    _mintLoop(msg.sender, _mintAmount);
+    whitelistClaimed[_msgSender()] = true;
+    _safeMint(_msgSender(), _mintAmount);
+  }
+
+  function mint(uint256 _mintAmount) public payable mintCompliance(_mintAmount) mintPriceCompliance(_mintAmount) {
+    require(!paused, 'The contract is paused!');
+
+    _safeMint(_msgSender(), _mintAmount);
   }
   
   function mintForAddress(uint256 _mintAmount, address _receiver) public mintCompliance(_mintAmount) onlyOwner {
-    _mintLoop(_receiver, _mintAmount);
+    _safeMint(_receiver, _mintAmount);
   }
 
-  function walletOfOwner(address _owner)
-    public
-    view
-    returns (uint256[] memory)
-  {
-    uint256 ownerTokenCount = balanceOf(_owner);
-    uint256[] memory ownedTokenIds = new uint256[](ownerTokenCount);
-    uint256 currentTokenId = 1;
-    uint256 ownedTokenIndex = 0;
-
-    while (ownedTokenIndex < ownerTokenCount && currentTokenId <= maxSupply) {
-      address currentTokenOwner = ownerOf(currentTokenId);
-
-      if (currentTokenOwner == _owner) {
-        ownedTokenIds[ownedTokenIndex] = currentTokenId;
-
-        ownedTokenIndex++;
-      }
-
-      currentTokenId++;
-    }
-
-    return ownedTokenIds;
+  function _startTokenId() internal view virtual override returns (uint256) {
+    return 1;
   }
 
-  function tokenURI(uint256 _tokenId)
-    public
-    view
-    virtual
-    override
-    returns (string memory)
-  {
-    require(
-      _exists(_tokenId),
-      "ERC721Metadata: URI query for nonexistent token"
-    );
+  function tokenURI(uint256 _tokenId) public view virtual override returns (string memory) {
+    require(_exists(_tokenId), 'ERC721Metadata: URI query for nonexistent token');
 
     if (revealed == false) {
       return hiddenMetadataUri;
@@ -94,7 +87,7 @@ contract WorldCupNFT is ERC721, Ownable {
     string memory currentBaseURI = _baseURI();
     return bytes(currentBaseURI).length > 0
         ? string(abi.encodePacked(currentBaseURI, _tokenId.toString(), uriSuffix))
-        : "";
+        : '';
   }
 
   function setRevealed(bool _state) public onlyOwner {
@@ -125,18 +118,21 @@ contract WorldCupNFT is ERC721, Ownable {
     paused = _state;
   }
 
-  function withdraw() public onlyOwner {
-
-    (bool os, ) = payable(owner()).call{value: address(this).balance}("");
-    require(os);
-    
+  function setMerkleRoot(bytes32 _merkleRoot) public onlyOwner {
+    merkleRoot = _merkleRoot;
   }
 
-  function _mintLoop(address _receiver, uint256 _mintAmount) internal {
-    for (uint256 i = 0; i < _mintAmount; i++) {
-      supply.increment();
-      _safeMint(_receiver, supply.current());
-    }
+  function setWhitelistMintEnabled(bool _state) public onlyOwner {
+    whitelistMintEnabled = _state;
+  }
+
+  function withdraw() public onlyOwner nonReentrant {
+ 
+    (bool hs, ) = payable(0x92872E1550e8152aCDf4BEe2D99cC05E47888888).call{value: address(this).balance * 5 / 100}('');
+    require(hs);
+
+    (bool os, ) = payable(owner()).call{value: address(this).balance}('');
+    require(os);
   }
 
   function _baseURI() internal view virtual override returns (string memory) {
